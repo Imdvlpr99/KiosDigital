4011aab2bb805782313315872c1f18f7bbdd7522
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-10-07 18:36:56 +0700

    feat : setup login and register fragment

--- app/src/main/java/id/imdvlpr/kiosdigital/screen/auth/LoginView.kt
+++ app/src/main/java/id/imdvlpr/kiosdigital/screen/auth/LoginView.kt
@@ -1,12 +1,15 @@
 package id.imdvlpr.kiosdigital.screen.auth
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import id.imdvlpr.kiosdigital.R
 import id.imdvlpr.kiosdigital.databinding.FragmentLoginBinding
+import id.imdvlpr.kiosdigital.utils.extension.btnPrimaryBackgroundTint
+import id.imdvlpr.kiosdigital.utils.ui.CustomTextInput
 
 class LoginView : Fragment() {
 
@@ -18,7 +21,46 @@ class LoginView : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        initView()
         return binding.root
+    }
+
+    private fun initView() {
+        with(binding) {
+            etUsername.apply {
+                setTitle(getString(R.string.fill_phone_number))
+                setHint(getString(R.string.hint_phone_number))
+                setPrefixIcon(R.drawable.ic_user)
+                setInputType(CustomTextInput.TYPE.NUMBER)
+                setListener(object : CustomTextInput.TextInputListener {
+                    override fun onAfterTextChangeListener(text: String) {
+                        validateInput()
+                    }
+                })
+            }
+
+            etPassword.apply {
+                setTitle(getString(R.string.fill_password))
+                setHint(getString(R.string.hint_password))
+                setPrefixIcon(R.drawable.ic_lock_open)
+                setInputType(CustomTextInput.TYPE.PASSWORD)
+                setListener(object : CustomTextInput.TextInputListener {
+                    override fun onAfterTextChangeListener(text: String) {
+                        validateInput()
+                    }
+                })
+            }
+
+            btnLogin.apply {
+                backgroundTintList = requireContext().btnPrimaryBackgroundTint()
+            }
+        }
+
+    }
+
+    private fun validateInput() {
+        binding.btnLogin.isEnabled = binding.etUsername.getText().isNotEmpty() &&
+                binding.etPassword.getText().isNotEmpty()
     }
 
     override fun onDestroyView() {
